3eaae9a2c15463a2d493d113b0b775bbc10e4382
raxelf <frananda.rafael@gmail.com>
2025-09-24 10:22:12 +0700

    feat: add icon

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,28 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import Logo from "@/assets/logo.png";
 import "./globals.css";
 import Navbar from "../components/Navbar";
 import { Toaster } from "sonner";
 import { cookies } from "next/headers";
 import { verifyToken } from "@/helpers/jwt";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
   title: "Learn Skill Map",
   description: "Platform buat bikin roadmap skill",
-  icons: {
-    icon: "/logo.png", 
-  },
 };
 
 export default async function RootLayout({
@@ -51,6 +36,26 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body>
         {token ? <Navbar userToken={token} /> : <Navbar />}
         <main>{children}</main>
