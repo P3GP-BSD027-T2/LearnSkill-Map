7ea330e2f3435112b326643943aa67589f5b7f10
raxelf <frananda.rafael@gmail.com>
2025-09-24 11:02:01 +0700

    fix: route handler /skills

--- client/src/app/api/skills/route.ts
+++ client/src/app/api/skills/route.ts
@@ -2,16 +2,8 @@ import axios from "axios";
 import { NextResponse } from "next/server";
 
 export const GET = async () => {
-  try {
-    const res = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/skills`);
-    // console.log(res.data);
+  const res = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/skills`);
+  // console.log(res.data);
 
-    return NextResponse.json(res.data);
-  } catch (error) {
-    console.error("Error fetching skills:", error);
-    return NextResponse.json(
-      { error: "Failed to fetch skills" },
-      { status: 500 }
-    );
-  }
+  return NextResponse.json(res.data);
 };
